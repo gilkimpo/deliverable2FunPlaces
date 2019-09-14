940c3e2990b765722ef10f7daff066e44f6a35a8
gil kimpo <gilkimpo@hotmail.com>
2019-09-14 18:59:35 -0400

    finalized program

--- Lab2deliverableFunPlaces/Program.cs
+++ Lab2deliverableFunPlaces/Program.cs
@@ -15,85 +15,108 @@ namespace Lab2deliverableFunPlaces
             Console.WriteLine("Please tell me your name.");
             userName = Console.ReadLine();
             Console.WriteLine(" ");
-            Console.WriteLine("Hello " + userName + " what are you in the mood for?");
-            Console.WriteLine("Please select the number of your choice from this list:");
-            Console.WriteLine("");
-            Console.WriteLine("1. Action");
-            Console.WriteLine("2. Chill");
-            Console.WriteLine("3. Danger");
-            Console.WriteLine("4. Food");
-            select1 = Convert.ToDouble(Console.ReadLine());
-
-
-            switch (select1)
+            do
             {
-                case 1:
+                Console.WriteLine("What are you in the mood for? Please select the number of your choice from this list:");
+                Console.WriteLine("");
+                Console.WriteLine("1. Action");
+                Console.WriteLine("2. Chill");
+                Console.WriteLine("3. Danger");
+                Console.WriteLine("4. Food");
+                select1 = Convert.ToDouble(Console.ReadLine());
+
+
+
+
+
+                if (select1 == 1)
+                {
                     Console.WriteLine("Aaaaaand ACTION!!");
                     Console.WriteLine("You should go Stock Car Racing!");
-                    break;
+                }
 
-                case 2:
+                if (select1 == 2)
+                {
                     Console.WriteLine("Lets' Chill");
                     Console.WriteLine("You should go hiking.");
-                    break;
+                }
 
-                case 3:
+                if (select1 == 3)
+                {
                     Console.WriteLine("DANGER Will Robinson!");
                     Console.WriteLine("You should go skydiving!");
-                    break;
+                }
 
-                case 4:
+                if (select1 == 4)
+                {
                     Console.WriteLine("I see...");
                     Console.WriteLine("You should go to Taco Bell.");
-                    break;
-                default:
-                    Console.WriteLine("Does not compute, please try again");
-                    break;
-            }      
+                }
+                if (select1 > 4)
+                {
+                    Console.WriteLine("Incorrect entry. Please try again.");
+                }
 
-                                                                                          
+            }
+            while (select1 > 4);
+            {
+                do
+                {
+
                     Console.WriteLine(" ");
                     Console.WriteLine("And how many people excluding yourself are going?");
                     Console.WriteLine("Please enter the number of your choice from this list:");
                     Console.WriteLine(" ");
-                    Console.WriteLine(" 1. 0");
+                    Console.WriteLine("1. 0");
                     Console.WriteLine("2. 1-4");
                     Console.WriteLine("3. 5-10");
                     Console.WriteLine("4. 11+");
-
                     select2 = Convert.ToDouble(Console.ReadLine());
 
-                    switch (select2)
+
+
+                    if (select2 == 1)
                     {
-                        case 1:
+                        Console.WriteLine("Well you should also go via sneakers, aka, walking there.");
+                    }
 
-                            Console.WriteLine("Well you should also go via sneakers, aka, walking there.");
-                            break;
-
-                        case 2:
-                            Console.WriteLine("You could also go via sedan, or maybe a Jeep!");
-                            break;
-
-                        case 3:
-                            Console.WriteLine("You should also definitely take a Volkswagen bus, not a clown car.");
-                            break;
-
-                        case 4:
-                            Console.WriteLine("You should also take an airplane, you baller.");
-                            break;
-
-                        default:
-                            Console.WriteLine("Does not compute, please try again");
-                            break;
-
-
-
+                    if (select2 == 2)
+                    {
+                        Console.WriteLine("You could also go via sedan, or maybe a Jeep!");
+                    }
+                    if (select2 == 3)
+                    {
+                        Console.WriteLine("You should also definitely take a Volkswagen bus, not a clown car.");
+                    }
+                    if (select2 == 4)
+                    {
+                        Console.WriteLine("You should also take an airplane, you baller.");
+                    }
+                    if (select2 > 4)
+                    {
+                        Console.WriteLine("Does not compute, please try again");
 
                     }
+
+                   
+                    
+
+                    
+                }
+                while (select2 > 4);
+                {
                     Console.WriteLine("");
                     Console.WriteLine("So long, farewell, Auf Wiedersehen, goodbye!  And have a great time!");
                     Console.ReadKey();
+                }
             }
+
+
         }
     }
+}
+            
+        
+    
+
 
